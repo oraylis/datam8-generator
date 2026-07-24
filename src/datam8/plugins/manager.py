dd1ef467fbba7662a4e0b1add21f0e0480ed46db
Fabio Kayser <f.kayser@oraylis.de>
2026-07-24 10:00:39 +0200

    refactor(plugins): require configured plugins path

--- src/datam8/plugins/manager.py
+++ src/datam8/plugins/manager.py
@@ -140,9 +140,7 @@ class PluginManager:
         """
         Load plugins from a solution's plugin directory into this PluginManager.
         """
-        plugins_path = config.solution_folder_path / (
-            solution.pluginsPath or Path("Plugins")
-        )
+        plugins_path = config.solution_folder_path / solution.pluginsPath
         self.__plugins_dir_path = plugins_path
 
         logger.debug("Plugins directory: %s", plugins_path.as_posix())
